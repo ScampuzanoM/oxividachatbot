ff213359d8b20d3765d54aa7f3c6b17bb6f7d7e9
ScampuzanoM <sebascm3450@gmail.com>
2024-04-29 09:11:52 -0500

    feat: :zap: Ajuste mensaje bienvendia

--- flows/welcome.flow.js
+++ flows/welcome.flow.js
@@ -9,17 +9,18 @@ const inactividad = require("./inactividad.flow")
  * Flujo de bienvenida
  */
 
-module.exports = addKeyword(['hola', 'ole', 'alo', 'buenas', 'menu', 'holi', 'hol', 'oe'])
+    module.exports = addKeyword(['hola', 'ole', 'alo', 'buenas', 'menu', 'holi', 'hol', 'oe'])
     // module.exports =  addKeyword('BOT')
-    .addAnswer('🙌 ¡Hola bienvenid@ a Oxivida!')
+    .addAnswer('Hola, 👋 bienvenid@ a la Clínica Oxivida. Somos un centro clínico de medicina complementaria especializado en terapias con oxígeno. Nuestro propósito es sanar con amor ❤️ y nos gusta cuidar de nuestros pacientes y de su tiempo, así que estamos aquí leyéndote.')
     .addAnswer('🙌 ¿Cual es tu nombre?', { capture: true, idle: Number(process.env.TIEMPO_INACTIVIDAD) },
-        async (ctx, { flowDynamic }) => {
+        async (ctx, { flowDynamic,gotoFlow }) => {
             if (ctx?.idleFallBack) {
                 return gotoFlow(inactividad)
-            } else {
-                const nombre = ctx.body
-                await flowDynamic(`De ahora en adelante te llamare ${nombre}`)
             }
+            // else {
+            //     const nombre = ctx.body
+            //     await flowDynamic(`De ahora en adelante te llamare ${nombre}`)
+            // }
         })
     .addAnswer(['¿Acepta terminos y condiciones?', '1. Si', '2. No'],
         { capture: true, idle: Number(process.env.TIEMPO_INACTIVIDAD) },
